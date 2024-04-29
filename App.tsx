4cd656c7689ffa6ee1972c897ed18df935550b6e
Phillipe <ph.stos.udia@gmail.com>
2024-04-29 20:45:05 -0300

    start-project-1c

--- App.tsx
+++ App.tsx
@@ -3,5 +3,6 @@ import Home from './src/screens/Home/Home';
 export default function App() {
   return (
       <Home/>
+      
   );
 }
---
+++ src/screens/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './MenuStyles';
+
+export function Menu() {
+  return (
+    <View style={styles.container}>
+        <Text>Tela Menu</Text>
+    </View>
+  );
+}
