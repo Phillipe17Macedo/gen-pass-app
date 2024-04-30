93d2954effeed90e2829b68d409dae6acc86be98
Phillipe <ph.stos.udia@gmail.com>
2024-04-29 21:06:27 -0300

    start-project-1e

--- App.tsx
+++ App.tsx
@@ -3,6 +3,5 @@ import Home from './src/screens/Home/Home';
 export default function App() {
   return (
       <Home/>
-      
   );
 }
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,12 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import styles from './Style';
+import { BatLogo } from '../BatLogo/BatLogo';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>TELA HOME </Text>
       <StatusBar style="auto" />
+
+      <View>
+        <BatLogo/>
+      </View>
+      
+      <Text>TELA HOME </Text>
     </View>
   );
 }
